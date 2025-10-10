51ab798ba1ba9a428fc498bcc7198f88a8c096da
Albert M. Canoy <80763419+Canz-143@users.noreply.github.com>
2025-10-10 10:19:01 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -6,3 +6,4 @@ load_dotenv()
 
 GOOGLE_API_KEY = "AIzaSyAdedPBgOlYjKVqV9GFQE2S-0pvm_SIjTM"
 FIRECRAWL_API_KEY = "fc-21af9fbec44f431ea254483c00aa514f"
+OPENAI_API_KEY = "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"
