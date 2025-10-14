fc465cee6fb5b16de9d2a1470800cf6a041d56e6
Albert M. Canoy <80763419+Canz-143@users.noreply.github.com>
2025-10-14 16:58:23 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GOOGLE_API_KEY = "AIzaSyAdedPBgOlYjKVqV9GFQE2S-0pvm_SIjTM"
+GOOGLE_API_KEY = "AIzaSyCd2mUUlRJuDT4Hn4ofYde9zUUwXp1DVYs"
 FIRECRAWL_API_KEY = "fc-21af9fbec44f431ea254483c00aa514f"
